abba6a2bf97bc5f0e4dec7f32050712465a65e43
OG-gopi <beesugopi@gmail.com>
2026-09-14 23:27:30 +0530

    fix: remove dist from git tracking to fix Vercel serverless routing conflict

--- dist/backend/dist/routes/auditRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auditController_1 = require("../controllers/auditController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.use(authMiddleware_1.authenticate);
-router.get('/', (0, authMiddleware_1.requireRole)('SUPER_ADMIN'), (0, authMiddleware_1.requirePermission)('audit_logs.view'), auditController_1.getAuditLogs);
-exports.default = router;
--- dist/backend/dist/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboardController_1 = require("../controllers/dashboardController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.use(authMiddleware_1.authenticate);
-router.get('/stats', dashboardController_1.getDashboardStats);
-exports.default = router;
